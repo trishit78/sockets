3d8fa3e1aed8681fa61852bce5b7b3f53334faf5
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-11-10 23:50:01 +0530

    feat: socket handshake auth with jwt verification

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,24 @@
 import express, { type Request, type Response } from 'express';
-
+import jwt from 'jsonwebtoken';
 import cors from 'cors'; 
 import { createServer } from 'node:http';
 import { Server  } from 'socket.io';
-import { serverConfig } from './config/index.js';
+import { authConfig, serverConfig } from './config/index.js';
 import { connectDB } from './config/db.js';
 import authRouter from './router/authRouter.js';
+import User from './models/User.js';
 //import { seedUser } from './seeders/seed.js';
 
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+      origin: "*", 
+      methods: ["GET", "POST"]
+    }
+  });
+  
 
 
 app.use(cors());
@@ -24,17 +31,55 @@ app.get('/health',(_req:Request,res:Response)=>{
     })
 });
 
+io.use(async(socket,next)=>{
+    const token  = socket.handshake.auth.token;
+    if (!token){
+        return next(new Error('Auth Error'))
+    } 
+    try {
+        const decoded = jwt.verify(token, authConfig.JWT_SECRET) as jwt.JwtPayload;
+        const userId = decoded.userId || decoded.id || decoded.sub
 
-io.on('connection',(socket)=>{
-    console.log('a user connected',socket.id);
-    socket.on('disconnect',()=>{
-        console.log('a user disconnected',socket.id)
-    })
+        if(!userId)    throw new Error('Invalid token payload');
+        // @ts-ignore
+        socket.userId = userId;
+            next();
+    } catch (err) {
+        next(new Error('Authentication failed'));
+    }
 })
 
+io.on('connection', async (socket) => {
+    try {
+        // @ts-ignore
+        const userId = socket.userId;
+        let userEmail = null;
+
+        if (userId) {
+            const user = await User.findById(userId).select('email');
+            userEmail = user ? user.email : null;
+        }
+
+        console.log('a user connected', socket.id, userEmail);
+
+        socket.on('connect_error',(error)=>{
+            if(error.message === 'Authentication error'){
+                console.log('Invalid token,please login again')
+            }
+        })
+
+        socket.on('disconnect', () => {
+            console.log('a user disconnected', socket.id,userEmail);
+        });
+    } catch (error) {
+        console.error('Error handling connection:', error);
+    }
+});
+
+
+await connectDB();
 server.listen(serverConfig.port, async()=>{
     console.log(`server is running on ${serverConfig.port}`)
-    await connectDB();
     //await seedUser()
     
 })
